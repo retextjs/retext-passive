779cb6ce67614e2c8839d57b55cc420bd69533f6
Titus Wormer <tituswormer@gmail.com>
2023-09-08 12:32:11 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,60 +1,50 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {retext} from 'retext'
 import retextPassive from './index.js'
 
-test('retext-passive', (t) => {
-  t.plan(3)
+test('retext-passive', async function (t) {
+  const doc = 'He was withheld while we were being fed.\nFed.\nThe fed.'
+  const file = await retext().use(retextPassive).process(doc)
 
-  const doc = [
-    'He was withheld while we were being fed.',
-    'Fed.',
-    'The fed.'
-  ].join('\n')
+  await t.test('should emit a message w/ metadata', async function () {
+    assert.deepEqual(JSON.parse(JSON.stringify(file.messages[0])), {
+      column: 8,
+      fatal: false,
+      message: 'Don’t use the passive voice',
+      line: 1,
+      name: '1:8-1:16',
+      place: {
+        start: {line: 1, column: 8, offset: 7},
+        end: {line: 1, column: 16, offset: 15}
+      },
+      reason: 'Don’t use the passive voice',
+      ruleId: 'withheld',
+      source: 'retext-passive',
+      actual: 'withheld',
+      expected: [],
+      url: 'https://github.com/retextjs/retext-passive#readme'
+    })
+  })
 
-  retext()
-    .use(retextPassive)
-    .process(doc)
-    .then((file) => {
-      t.deepEqual(
-        file.messages.map(String),
-        [
-          '1:8-1:16: Don’t use the passive voice',
-          '1:37-1:40: Don’t use the passive voice'
-        ],
-        'should work'
-      )
+  await t.test('should emit messasges', async function () {
+    assert.deepEqual(
+      file.messages.map(String),
+      [
+        '1:8-1:16: Don’t use the passive voice',
+        '1:37-1:40: Don’t use the passive voice'
+      ],
+      'should work'
+    )
+  })
 
-      t.deepEqual(
-        JSON.parse(JSON.stringify(file.messages[0])),
-        {
-          column: 8,
-          fatal: false,
-          message: 'Don’t use the passive voice',
-          line: 1,
-          name: '1:8-1:16',
-          place: {
-            start: {line: 1, column: 8, offset: 7},
-            end: {line: 1, column: 16, offset: 15}
-          },
-          reason: 'Don’t use the passive voice',
-          ruleId: 'withheld',
-          source: 'retext-passive',
-          actual: 'withheld',
-          expected: [],
-          url: 'https://github.com/retextjs/retext-passive#readme'
-        },
-        'should emit messages'
-      )
-    }, t.ifErr)
+  await t.test('should `ignore`', async function () {
+    const file = await retext()
+      .use(retextPassive, {ignore: ['fed']})
+      .process(doc)
 
-  retext()
-    .use(retextPassive, {ignore: ['fed']})
-    .process(doc)
-    .then((file) => {
-      t.deepEqual(
-        file.messages.map(String),
-        ['1:8-1:16: Don’t use the passive voice'],
-        'should `ignore`'
-      )
-    }, t.ifErr)
+    assert.deepEqual(file.messages.map(String), [
+      '1:8-1:16: Don’t use the passive voice'
+    ])
+  })
 })
