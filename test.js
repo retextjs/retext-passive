57c5f10f389dd33a0edb9f605c92c2a70dcc40a1
Titus Wormer <tituswormer@gmail.com>
2023-09-08 12:41:02 +0200

    Change to improve message

--- test.js
+++ test.js
@@ -14,34 +14,34 @@ test('retext-passive', async function (t) {
   const file = await retext().use(retextPassive).process(doc)
 
   await t.test('should emit a message w/ metadata', async function () {
-    assert.deepEqual(JSON.parse(JSON.stringify(file.messages[0])), {
-      column: 8,
-      fatal: false,
-      message: 'Don’t use the passive voice',
-      line: 1,
-      name: '1:8-1:16',
-      place: {
-        start: {line: 1, column: 8, offset: 7},
-        end: {line: 1, column: 16, offset: 15}
-      },
-      reason: 'Don’t use the passive voice',
-      ruleId: 'withheld',
-      source: 'retext-passive',
-      actual: 'withheld',
-      expected: [],
-      url: 'https://github.com/retextjs/retext-passive#readme'
-    })
+    assert.deepEqual(
+      JSON.parse(JSON.stringify({...file.messages[0], ancestors: []})),
+      {
+        ancestors: [],
+        column: 8,
+        fatal: false,
+        message: 'Unexpected use of the passive voice',
+        line: 1,
+        name: '1:8-1:16',
+        place: {
+          start: {line: 1, column: 8, offset: 7},
+          end: {line: 1, column: 16, offset: 15}
+        },
+        reason: 'Unexpected use of the passive voice',
+        ruleId: 'withheld',
+        source: 'retext-passive',
+        actual: 'withheld',
+        expected: [],
+        url: 'https://github.com/retextjs/retext-passive#readme'
+      }
+    )
   })
 
   await t.test('should emit messasges', async function () {
-    assert.deepEqual(
-      file.messages.map(String),
-      [
-        '1:8-1:16: Don’t use the passive voice',
-        '1:37-1:40: Don’t use the passive voice'
-      ],
-      'should work'
-    )
+    assert.deepEqual(file.messages.map(String), [
+      '1:8-1:16: Unexpected use of the passive voice',
+      '1:37-1:40: Unexpected use of the passive voice'
+    ])
   })
 
   await t.test('should `ignore`', async function () {
@@ -50,7 +50,7 @@ test('retext-passive', async function (t) {
       .process(doc)
 
     assert.deepEqual(file.messages.map(String), [
-      '1:8-1:16: Don’t use the passive voice'
+      '1:8-1:16: Unexpected use of the passive voice'
     ])
   })
 })
