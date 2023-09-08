43afe9a972fd18a07de33e15db667eb1fd3a3821
Titus Wormer <tituswormer@gmail.com>
2023-09-08 12:45:43 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {retext} from 'retext'
-import retextPassive from './index.js'
+import retextPassive from 'retext-passive'
 
 test('retext-passive', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('retext-passive')).sort(), [
       'default'
     ])
   })
